e753cb8c60bfb40c8aa100b9d31b5c1c643460e2
gerrymonga <mongagerry51@gmail.com>
2025-04-14 14:45:48 +0530

    V3fix3

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-  origin:'http://localhost:3000',
+  origin:'https://transcendent-alpaca-5082ae.netlify.app/',
   credentials:true,
 }));
 app.use(cookieParser())
